d98fc9d314933ffe366dd8aac1d0dfece1ef3fef
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-24 16:13:33 +0100

    [8] Minor layout changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // const navElems = {
-//   nav: {
+//   navItems: {
 //     className: 'nav',
-//     id: 'nav'
 //   },
 //   navList: {
 //     className: 'nav__list',
-//     id: 'nav_list'
 //   }
 // };
 //
 // const toggleNav = obj => {
-//   let el = document.getElementById(obj.id);
+//   let elList = document.getElementById(obj.className);
 //   let expanded = `${obj.className}--expanded`;
-//   el.classList.contains(expanded) ? el.classList.remove(expanded) : el.classList.add(expanded);
+//   for (let el in elList) {
+//    toggleClass(el, expanded);
+//   }
 // }
 //
 // const toggleNavElems = document.getElementsByClassName('toggle-nav');
@@ -25,20 +25,17 @@
 // }
 
 
-// Function that takes arguments of element and class and
-// adds it to/ removes it from that element:
-const toggleClass = (el, className) => {
-  el.classList.contains(className) ?
-  el.classList.remove(className) : el.classList.add(className);
-}
-
 // All elements with 'toggle-nav' class now have event listener
-// that expands navigation on small screen (by calling toggleClass function that will
-// add 'nav__list--expanded class' to #nav_list):
+// that expands navigation on small screen:
 const toggleNavElems = document.getElementsByClassName('toggle-nav');
+const navList = document.getElementById('nav_list');
+const navItems = document.getElementsByClassName('nav__item');
+
 for (let toggler of toggleNavElems) {
   toggler.addEventListener('click', () => {
-    let navList = document.getElementById('nav_list');
-    toggleClass(navList, 'nav__list--expanded');
+    navList.classList.toggle('nav__list--expanded');
+    for (let item of navItems) {
+      item.classList.toggle('nav__item--expanded');
+    }
   })
 }
