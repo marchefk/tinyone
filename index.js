e310918b86628396e444c858de969c5f4563d6f7
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-24 12:49:32 +0100

    [7] Mobile menu expanding

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-const navElems = {
-  nav: {
-    className: 'nav',
-    id: 'nav'
-  },
-  navList: {
-    className: 'nav__list',
-    id: 'nav_list'
-  }
-};
+// const navElems = {
+//   nav: {
+//     className: 'nav',
+//     id: 'nav'
+//   },
+//   navList: {
+//     className: 'nav__list',
+//     id: 'nav_list'
+//   }
+// };
+//
+// const toggleNav = obj => {
+//   let el = document.getElementById(obj.id);
+//   let expanded = `${obj.className}--expanded`;
+//   el.classList.contains(expanded) ? el.classList.remove(expanded) : el.classList.add(expanded);
+// }
+//
+// const toggleNavElems = document.getElementsByClassName('toggle-nav');
+// for (let toggler of toggleNavElems) {
+//   toggler.addEventListener('click', () => {
+//     for (var e in navElems) {
+//       toggleNav(navElems[e]);
+//     }
+//   });
+// }
 
-const toggleNav = obj => {
-  let el = document.getElementById(obj.id);
-  let expanded = `${obj.className}--expanded`;
-  el.classList.contains(expanded) ? el.classList.remove(expanded) : el.classList.add(expanded);
+
+// Function that takes arguments of element and class and
+// adds it to/ removes it from that element:
+const toggleClass = (el, className) => {
+  el.classList.contains(className) ?
+  el.classList.remove(className) : el.classList.add(className);
 }
 
+// All elements with 'toggle-nav' class now have event listener
+// that expands navigation on small screen (by calling toggleClass function that will
+// add 'nav__list--expanded class' to #nav_list):
 const toggleNavElems = document.getElementsByClassName('toggle-nav');
 for (let toggler of toggleNavElems) {
   toggler.addEventListener('click', () => {
-    for (var e in navElems) {
-      toggleNav(navElems[e]);
-    }
-  });
+    let navList = document.getElementById('nav_list');
+    toggleClass(navList, 'nav__list--expanded');
+  })
 }
