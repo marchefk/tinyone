9649a115945a13bda12d7074590511598b1f617d
Ewa Jackiewicz <jackiewicz.ewa@wp.pl>
2019-01-25 15:08:21 +0100

    [11] Slideshow

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ const getAction = (a, b, el, className) => {
 
 // On mobile when user scrolls down, navigation will change its position so that
 // its not visible. When the user scrolls up it will appear again.
+let prevScrollPos = window.pageYOffset;
+const scrollHideEls = [nav, navButton, navList];
 const hideNav = currPos => {
   for (let el of scrollHideEls) {
     let action = getAction(prevScrollPos, currPos, el, 'scroll-hide');
@@ -69,7 +71,7 @@ const hideNav = currPos => {
 
 
 // Shrink nav height after scrolling past a certain point (screen > 800px):
-const shrinkNavPoint = 1/5 * document.getElementById('section_home').clientHeight;
+const shrinkNavPoint = 1/10 * document.getElementById('section_home').clientHeight;
 const shrinkNav = currPos => {
   let action = getAction(shrinkNavPoint, currPos, nav, 'nav--shrink');
   if (action) {
@@ -78,11 +80,31 @@ const shrinkNav = currPos => {
 }
 
 
+// Slideshow function that gets called every 3 seconds
+let slideIndex = 0;
+const startSlideshow = () => {
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+  slideIndex += 1;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let dot of dots) {
+    dot.className = dot.className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+  setTimeout(startSlideshow, 3000);
+}
+
+
 // shrinkNav and hideNav added to window.onscroll:
-let prevScrollPos = window.pageYOffset;
-const scrollHideEls = [nav, navButton, navList];
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
   hideNav(currentScrollPos);
   shrinkNav(currentScrollPos);
 }
+startSlideshow();
